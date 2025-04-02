c6952f28e02f21451bb2b15b9957d52dff5d87c6
M1K45 <272570@student.pwr.edu.pl>
2025-04-03 00:08:36 +0200

    Nie wiem dlaczego nie mogę commitować innych projektów niż ten, zapytam się na labach jak

--- Program.cs
+++ Program.cs
@@ -3,6 +3,14 @@ using System.Text.Json;
 using System.Text.Json.Serialization;
 using System;
 using System.Threading.Tasks;
+using database_app;
+
+/*
+ * Pytania do prowadzącego 
+ * - problem z bazą danych - nie mogę jej utworzyć
+ * - jak dodać na gita kolejny projekt, jeśli go się utworzyło? 
+ */ 
+
 
 namespace database_app
 {
@@ -31,3 +39,33 @@ namespace database_app
         }
     }
 }
+
+//using database_app;
+//using System.Text.Json;
+
+//namespace WinFormsApp1
+//{
+//    public partial class Form1 : Form
+//    {
+//        private HttpClient client;
+//        public Form1()
+//        {
+//            InitializeComponent();
+//            client = new HttpClient();
+//        }
+
+//        private async void button1_Click(object sender, EventArgs e)
+//        {
+//            string key = "35191f5f03089db2dc2d68f960ec6bcb";
+//            string call = $"https://api.openweathermap.org/data/2.5/weather?q=Wroclaw&appid={key}";
+//            string response = await client.GetStringAsync(call);
+//            Weather weather = JsonSerializer.Deserialize<Weather>(response);
+//            listBox1.Items.Add(weather.ToString());
+//        }
+
+//        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+//        {
+
+//        }
+//    }
+//}
